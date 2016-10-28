b0dcc33bce9dce1b50aa2b9593e533251f2f7fbc
Antônio <Antnio@172.16.26.195>
2016-10-28 19:43:14 -0200

    Finalizado conversão de String para Data. Inciado geração de logs do sistema.

--- src/br/unesc/reserva/modelo/Generics.java
+++ src/br/unesc/reserva/modelo/Generics.java
@@ -1,16 +1,32 @@
 package br.unesc.reserva.modelo;
 
+import java.io.*;
+import java.util.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Generics {
-    /* public static Date formataData(String data) throws Exception { 
-     if (data == null || data.equals(""))
-     return null;
-     Date date = null;
-     try {
-     DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-     date = (java.util.Date)formatter.parse(data);
-     } catch (ParseException e) {            
-     throw e;
-     }
-     return date;
-     } */
+
+    public static Date formataData(String data) throws Exception {
+        if (data == null || data.equals("")) {
+            return null;
+        }
+        Date date = null;
+        try {
+            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            date = (java.util.Date) formatter.parse(data);
+        } catch (ParseException e) {
+            throw e;
+        }
+        return date;
+    }
+
+    public void GerarLog(String mensagem) {
+
+        String local = "../src/log/log.txt";
+
+        File arquivoLog = new File(local);
+
+    }
 }
--- src/br/unesc/reserva/modelo/ReservaAction.java
+++ src/br/unesc/reserva/modelo/ReservaAction.java
@@ -1,20 +1,27 @@
 package br.unesc.reserva.modelo;
 
+import br.unesc.reserva.view.JanelaIReserva;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 
 public class ReservaAction implements ActionListener{
 
+    JanelaIReserva frame;
+    
+    public ReservaAction(JanelaIReserva frame) {
+        this.frame = frame;
+    }
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         
         if("salvar".equals(e.getActionCommand())){
-            JOptionPane.showMessageDialog(null,"Clicado no salvar!");
+            Reserva r = frame.getReserva();
         }
         
         if("editar".equals(e.getActionCommand())){
-            JOptionPane.showMessageDialog(null,"Clicado no editar!");
+            Reserva r = frame.getReserva();
         }
         
         if("excluir".equals(e.getActionCommand())){
