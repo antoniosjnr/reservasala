c062f7fb1ca4ce577b21048e2a0e82153a70ca3f
jeff <jeff@jeff-PC>
2016-10-29 21:20:32 -0200

    adicionado o metado insertString para restringir entradas de dados

--- src/br/unesc/reserva/view/JanelaLogin.java
+++ src/br/unesc/reserva/view/JanelaLogin.java
@@ -49,6 +49,8 @@ public class JanelaLogin extends javax.swing.JFrame {
         lblSenha = new javax.swing.JLabel();
         txtSenha = new javax.swing.JPasswordField();
         btnEntrar = new javax.swing.JButton();
+        lblLogin1 = new javax.swing.JLabel();
+        lblSenha1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -81,35 +83,59 @@ public class JanelaLogin extends javax.swing.JFrame {
             }
         });
 
+        lblLogin1.setBackground(new java.awt.Color(50, 50, 50));
+        lblLogin1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lblLogin1.setForeground(new java.awt.Color(255, 255, 255));
+        lblLogin1.setIcon(new javax.swing.ImageIcon("C:\\Users\\jeff\\Downloads\\aa.jpg")); // NOI18N
+
+        lblSenha1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lblSenha1.setForeground(new java.awt.Color(255, 255, 255));
+        lblSenha1.setText("Sistema de Reserva Salas");
+
         javax.swing.GroupLayout painelFundoLayout = new javax.swing.GroupLayout(painelFundo);
         painelFundo.setLayout(painelFundoLayout);
         painelFundoLayout.setHorizontalGroup(
             painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(painelFundoLayout.createSequentialGroup()
                 .addContainerGap()
+                .addComponent(lblLogin1, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(lblLogin)
-                        .addComponent(lblSenha)
-                        .addComponent(txtLogin, javax.swing.GroupLayout.DEFAULT_SIZE, 166, Short.MAX_VALUE)
-                        .addComponent(txtSenha))
-                    .addComponent(btnEntrar))
-                .addContainerGap(33, Short.MAX_VALUE))
+                    .addGroup(painelFundoLayout.createSequentialGroup()
+                        .addGap(49, 49, 49)
+                        .addComponent(lblSenha1))
+                    .addGroup(painelFundoLayout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(btnEntrar)
+                            .addGroup(painelFundoLayout.createSequentialGroup()
+                                .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(lblSenha)
+                                    .addComponent(lblLogin))
+                                .addGap(18, 18, 18)
+                                .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtSenha, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                .addContainerGap(31, Short.MAX_VALUE))
         );
         painelFundoLayout.setVerticalGroup(
             painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(painelFundoLayout.createSequentialGroup()
-                .addGap(31, 31, 31)
-                .addComponent(lblLogin)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(lblSenha)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtSenha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(btnEntrar)
-                .addContainerGap(19, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painelFundoLayout.createSequentialGroup()
+                .addContainerGap(24, Short.MAX_VALUE)
+                .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblLogin1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(painelFundoLayout.createSequentialGroup()
+                        .addComponent(lblSenha1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblLogin)
+                            .addComponent(txtLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblSenha)
+                            .addComponent(txtSenha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnEntrar)))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -120,11 +146,62 @@ public class JanelaLogin extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(painelFundo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(painelFundo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnEntrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEntrarActionPerformed
+
+        if (evt.getSource() == btnEntrar) {
+
+            File arquivo = new File("USUARIO.txt");
+
+            String caminho = arquivo.getAbsolutePath();
+            //ESCREVE O LOGIN
+            try {
+                PrintWriter arq = new PrintWriter(caminho);
+                arq.println(txtLogin.getText());
+                arq.close();
+                System.out.println("Escrveu no arquivo");
+            } catch (Exception ex) {
+                System.out.println("Nao escrveu no arquivo \nErro: " + ex);
+            }
+        }
+
+        JanelaPrincipal jp = null;
+        try {
+            jp = new JanelaPrincipal();
+        } catch (IOException ex) {
+            Logger.getLogger(JanelaLogin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        jp.setVisible(true);
+        this.dispose();
+
+        String usuario = null;
+        try {
+            usuario = Generics.getUsuario();
+        } catch (IOException ex) {
+            Logger.getLogger(JanelaLogin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            Generics.GerarLog("Entrou no sistema",usuario);
+        } catch (IOException ex) {
+            Logger.getLogger(JanelaLogin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_btnEntrarActionPerformed
+
+    private void txtSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtSenhaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtSenhaActionPerformed
+
+    private void txtLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtLoginActionPerformed
+
+    }//GEN-LAST:event_txtLoginActionPerformed
 
     private void analizar() {
 
@@ -157,47 +234,6 @@ public class JanelaLogin extends javax.swing.JFrame {
         
     }
 
-    private void btnEntrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEntrarActionPerformed
-        
-        if (evt.getSource() == btnEntrar) {
-
-            File arquivo = new File("USUARIO.txt");
-            
-            String caminho = arquivo.getAbsolutePath();
-            //ESCREVE O LOGIN           
-                try {
-                    PrintWriter arq = new PrintWriter(caminho);
-                    arq.println(txtLogin.getText());
-                    arq.close();
-                    System.out.println("Escrveu no arquivo");
-                } catch (Exception ex) {
-                    System.out.println("Nao escrveu no arquivo \nErro: " + ex);
-                }
-           }
-        
-        JanelaPrincipal jp = null;
-        try {
-            jp = new JanelaPrincipal();
-        } catch (IOException ex) {
-            Logger.getLogger(JanelaLogin.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        jp.setVisible(true);
-        this.dispose();
-        
-        String usuario = null;
-        try {
-            usuario = Generics.getUsuario();
-        } catch (IOException ex) {
-            Logger.getLogger(JanelaLogin.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        try {
-            Generics.GerarLog("Entrou no sistema",usuario);
-        } catch (IOException ex) {
-            Logger.getLogger(JanelaLogin.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_btnEntrarActionPerformed
-
         public class teclasletras extends PlainDocument {
 
         @Override
@@ -212,19 +248,13 @@ public class JanelaLogin extends javax.swing.JFrame {
                     throws BadLocationException {
                 super.insertString(offset, str.replaceAll("[^0-9]",""), attr);}}
     
-
-    private void txtLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtLoginActionPerformed
-        
-    }//GEN-LAST:event_txtLoginActionPerformed
-
-    private void txtSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtSenhaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtSenhaActionPerformed
  
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnEntrar;
     private javax.swing.JLabel lblLogin;
+    private javax.swing.JLabel lblLogin1;
     private javax.swing.JLabel lblSenha;
+    private javax.swing.JLabel lblSenha1;
     private javax.swing.JPanel painelFundo;
     private javax.swing.JTextField txtLogin;
     private javax.swing.JPasswordField txtSenha;
