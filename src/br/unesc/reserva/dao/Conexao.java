c97aa6052b034a4e0f71416f0844681d6ff82296
comp1 <comp1@comp1_05.lab.unesc.net>
2016-11-11 19:54:38 -0200

    Realizado inserção de banco de dados hsqldb, criado a modelagem do banco de dados e criado a classe responsável pela conexão ao banco de dados.

---
+++ src/br/unesc/reserva/dao/Conexao.java
@@ -0,0 +1,22 @@
+package br.unesc.reserva.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("org.hsqldb.jdbcDriver");
+            conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/reservasaladb", "sa", "");
+        } catch (SQLException e) {
+            System.out.println("Problemas ao conectar no banco de dados");
+        } catch (ClassNotFoundException e) {
+            System.out.println("O driver não foi configurado corretametne");
+        }
+
+        return conn;
+    }
+}
