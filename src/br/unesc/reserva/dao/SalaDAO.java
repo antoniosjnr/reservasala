608d8ecd211c9a9a971dbb0621818be9ae9a847d
comp1 <comp1@comp1_06.lab.unesc.net>
2016-11-18 21:19:24 -0200

    criou-se a classe SALA pra conexão

---
+++ src/br/unesc/reserva/dao/SalaDAO.java
@@ -0,0 +1,27 @@
+
+package br.unesc.reserva.dao;
+
+
+
+public class SalaDAO {
+    private final String INSERT = "insert to Sala( codigo,bloco,andar) valeus (?,?,?)";
+    private final String UPDATE = "update sala set id_codigo = ?, id_bloco = ?,id_andar = ?";
+    private final String DELETE = " delete from sala where codigo = ?";
+    private final String GET_SALA = "select  * from sala where codigo = ?";
+    
+    
+    
+    public void insert(){
+        
+    }
+    public void update(){
+        
+    }
+    
+    public void delete(){
+        
+    }
+    public void sala(){
+        
+    }
+}
